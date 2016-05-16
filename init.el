855b20e8ea1f1e6e101d0d5fd41bd7d3656d54d6
Sriram Krishnaswamy <sriram.krish.92@gmail.com>
2016-05-16 03:08:12 -0400

    Tracking master

--- init.el
+++ init.el
@@ -6,16 +6,12 @@
 
 ;;; Code:
 
-;; Increase the garbage collection threshold to 200 MB to ease startup
-(setq gc-cons-threshold (* 200 1024 1024))
-
-;; Initialize the packages but don't enable them at start
-(package-initialize nil)
-(setq package-enable-at-startup nil)
+;; Increase the garbage collection threshold to 500 MB to ease startup
+(setq gc-cons-threshold (* 500 1024 1024))
 
 ;; Load config.org - my Emacs configuration
 (org-babel-load-file (concat user-emacs-directory "config.org"))
 
-;; Garbage collector - decrease threshold
-(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold (* 800 1024))))
-;;; init.el ends here
+;; Garbage collector - decrease threshold to 5 MB
+(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold (* 5 1024 1024))))
+  ;;; init.el ends here

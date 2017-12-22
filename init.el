e61f38ba98360b28829bab280d0160bf88cb5b67
Sriram Krishnaswamy <sriram.krish.92@gmail.com>
2017-12-21 22:34:19 -0500

    Add avy and related packages
    This is to simulate mouse click based on various characters/lines/words

--- init.el
+++ init.el
@@ -323,6 +323,44 @@
   (which-key-enable-god-mode-support)
   (which-key-mode))
 
+;; simulating mouse click
+(use-package avy
+  :ensure t
+  :commands
+  (avy-goto-char-2
+   avy-goto-word-1
+   avy-goto-char-2-above
+   avy-goto-char-2-below
+   avy-goto-char-in-line
+   avy-goto-line)
+  :init
+  (setq avy-keys-alist
+		`((avy-goto-char-2			. (?j ?k ?l ?f ?s ?d ?e ?r ?u ?i))
+		  (avy-goto-word-1			. (?j ?k ?l ?f ?s ?d ?e ?r ?u ?i))
+		  (avy-goto-char-in-line	. (?j ?k ?l ?f ?s ?d ?e ?r ?u ?i))
+		  (avy-goto-char-2-above	. (?j ?k ?l ?f ?s ?d ?e ?r ?u ?i))
+		  (avy-goto-char-2-below	. (?j ?k ?l ?f ?s ?d ?e ?r ?u ?i))
+		  (avy-goto-line			. (?j ?k ?l ?f ?s ?d ?e ?r ?u ?i))))
+  ;; (setq avy-style 'pre)
+  (setq avy-background t)
+  (defface avy-lead-face-0
+	'((t (:foreground "white" :background "color-21")))
+	"Face used for first non-terminating leading chars."))
+
+;; jump to windows quickly
+(use-package ace-window
+  :ensure t
+  :after (avy)
+  :commands
+  (ace-window))
+
+;; jump and open links fast
+(use-package ace-link
+  :ensure t
+  :after (avy)
+  :config
+  (ace-link-setup-default))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;;    Key bindings    ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;

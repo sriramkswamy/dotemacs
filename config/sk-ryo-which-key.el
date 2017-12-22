88b24024d1e57b114878fc5e8dfb72ff0b65f1a4
Sriram Krishnaswamy <sriram.krish.92@gmail.com>
2017-12-22 09:54:36 -0500

    Hints for bindings in ryo-modal mode

--- config/sk-ryo-which-key.el
+++ config/sk-ryo-which-key.el
@@ -1,9 +1,38 @@
-;; operators
+;; basic operators
 (which-key-add-key-based-replacements
   "c" "change"
+  "C" "change rest of line"
+  "c i" "inside"
+  "c a" "around"
+  "c c" "change line or region"
+
   "d" "delete"
+  "D" "delete rest of line"
+  "d a" "inside"
+  "d i" "around"
+  "d d" "delete line/region"
+
   "v" "select"
-  "y" "copy")
+  "V" "start select"
+  "v i" "inside"
+  "v a" "around"
+  "v v" "expand region"
+
+  "y" "copy"
+  "Y" "copy rest of line"
+  "y i" "inside"
+  "y a" "around"
+  "y y" "copy line/region")
+
+;; movement
+(which-key-add-key-based-replacements
+  "w" "window")
+
+;; prefixes
+(which-key-add-key-based-replacements
+  "m" "mode"
+  "g" "global"
+  "SPC" "leader")
 
 ;; general functions
 (which-key-add-key-based-replacements

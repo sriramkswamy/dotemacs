8876e8ff3f422c8c90a1afd02337cbcb7fbaa0c5
Sriram Krishnaswamy <sriram.krish.92@gmail.com>
2017-12-26 21:24:50 -0800

    Move latex to a global map
    Not able to figure out how to do a local map for now properly with use package
    and ryo-modal

--- lang/sk-latex.el
+++ lang/sk-latex.el
@@ -5,12 +5,11 @@
   (let ((current-prefix-arg 4)) ;; emulate C-u
 	(call-interactively 'reftex-reference)))
 
-(use-package tex-site
+(use-package latex
   :ensure auctex
-  :ensure auctex-latexmk
-  :ensure latex-preview-pane
+  :defer t
   :diminish reftex-mode
-  :defer 1
+  :hook ((latex-mode . sk/setup-ryo-latex-bindings))
   
   :commands
   (LaTeX-mark-section
@@ -21,9 +20,27 @@
    reftex-view-crossref
    reftex-toc-recenter
    reftex-index
-   latex-preview-pane
-   latex-preview-pane-enable
-   latex-preview-pane-mode)
+   preview-at-point
+   preview-buffer
+   LaTeX-fill-section
+   LaTeX-fill-environment
+   LaTeX-fill-paragraph
+   LaTeX-fill-region
+   reftex-reference
+   reftex-label
+   reftex-citation
+   reftex-view-crossref
+   reftex-toc-recenter
+   reftex-index
+   LaTeX-insert-environment
+   LaTeX-section
+   TeX-view
+   TeX-master-file-ask
+   TeX-home-buffer
+   TeX-command-run-all
+   TeX-recenter-output-buffer
+   TeX-kill-job
+   preview-clearout-buffer)
   :mode (("\\.tex\\'" . latex-mode)
 		 ("\\.xtx\\'" . latex-mode))
   
@@ -70,92 +87,33 @@
   ;; turn on reftex
   (reftex-mode))
 
-;; bindings
-(ryo-modal-major-mode-keys
- 'latex-mode
- ("m x" preview-at-point :name "preview at point")
- ("m a" preview-buffer :name "preview buffer")
- ("m p" latex-preview-pane-mode :name "preview pane")
- ("m q s" LaTeX-fill-section :name "fill section")
- ("m q e" LaTeX-fill-environment :name "fill env")
- ("m q p" LaTeX-fill-paragraph :name "fill para")
- ("m q q" LaTeX-fill-region :name "fill region")
- ("m r" reftex-reference :name "ref")
- ("m l" reftex-label :name "label")
- ("m c" reftex-citation :name "cite")
- ("m j" reftex-view-crossref :name "crossrefs")
- ("m t" reftex-toc-recenter :name "table of contents")
- ("m i" reftex-index :name "index")
- ("m e" LaTeX-insert-environment :name "environment")
- ("m s" LaTeX-section :name "section")
- ("m v" TeX-view :name "view")
- ("m f" TeX-master-file-ask :name "master file")
- ("m h" TeX-home-buffer :name "home buffer")
- ("m m" TeX-command-run-all :name "compile")
- ("m o" TeX-recenter-output-buffer :name "compile output")
- ("m k" TeX-kill-job :name "kill compilation")
- ("m d" preview-clearout-buffer :name "clear preview"))
+;; latex mode bindings
+(ryo-modal-keys
+ ("\\ x" preview-at-point :name "preview at point")
+ ("\\ a" preview-buffer :name "preview buffer")
+ ("\\ q s" LaTeX-fill-section :name "fill section")
+ ("\\ q e" LaTeX-fill-environment :name "fill env")
+ ("\\ q p" LaTeX-fill-paragraph :name "fill para")
+ ("\\ q q" LaTeX-fill-region :name "fill region")
+ ("\\ r" reftex-reference :name "ref")
+ ("\\ l" reftex-label :name "label")
+ ("\\ c" reftex-citation :name "cite")
+ ("\\ j" reftex-view-crossref :name "crossrefs")
+ ("\\ t" reftex-toc-recenter :name "table of contents")
+ ("\\ i" reftex-index :name "index")
+ ("\\ e" LaTeX-insert-environment :name "environment")
+ ("\\ s" LaTeX-section :name "section")
+ ("\\ v" TeX-view :name "view")
+ ("\\ f" TeX-master-file-ask :name "master file")
+ ("\\ h" TeX-home-buffer :name "home buffer")
+ ("\\ \\" TeX-command-run-all :name "compile")
+ ("\\ o" TeX-recenter-output-buffer :name "compile output")
+ ("\\ k" TeX-kill-job :name "kill compilation")
+ ("\\ d" preview-clearout-buffer :name "clear preview"))
 ;; hints
-(which-key-add-major-mode-key-based-replacements 'latex-mode
-  "m q" "fill")
-
-;; bindings
-(ryo-modal-major-mode-keys
- 'LaTeX-mode
- ("m x" preview-at-point :name "preview at point")
- ("m a" preview-buffer :name "preview buffer")
- ("m p" latex-preview-pane-mode :name "preview pane")
- ("m q s" LaTeX-fill-section :name "fill section")
- ("m q e" LaTeX-fill-environment :name "fill env")
- ("m q p" LaTeX-fill-paragraph :name "fill para")
- ("m q q" LaTeX-fill-region :name "fill region")
- ("m r" reftex-reference :name "ref")
- ("m l" reftex-label :name "label")
- ("m c" reftex-citation :name "cite")
- ("m j" reftex-view-crossref :name "crossrefs")
- ("m t" reftex-toc-recenter :name "table of contents")
- ("m i" reftex-index :name "index")
- ("m e" LaTeX-insert-environment :name "environment")
- ("m s" LaTeX-section :name "section")
- ("m v" TeX-view :name "view")
- ("m f" TeX-master-file-ask :name "master file")
- ("m h" TeX-home-buffer :name "home buffer")
- ("m m" TeX-command-run-all :name "compile")
- ("m o" TeX-recenter-output-buffer :name "compile output")
- ("m k" TeX-kill-job :name "kill compilation")
- ("m d" preview-clearout-buffer :name "clear preview"))
-;; hints
-(which-key-add-major-mode-key-based-replacements 'LaTeX-mode
-  "m q" "fill")
-
-;; bindings
-(ryo-modal-major-mode-keys
- 'TeX-mode
- ("m x" preview-at-point :name "preview at point")
- ("m a" preview-buffer :name "preview buffer")
- ("m p" latex-preview-pane-mode :name "preview pane")
- ("m q s" LaTeX-fill-section :name "fill section")
- ("m q e" LaTeX-fill-environment :name "fill env")
- ("m q p" LaTeX-fill-paragraph :name "fill para")
- ("m q q" LaTeX-fill-region :name "fill region")
- ("m r" reftex-reference :name "ref")
- ("m l" reftex-label :name "label")
- ("m c" reftex-citation :name "cite")
- ("m j" reftex-view-crossref :name "crossrefs")
- ("m t" reftex-toc-recenter :name "table of contents")
- ("m i" reftex-index :name "index")
- ("m e" LaTeX-insert-environment :name "environment")
- ("m s" LaTeX-section :name "section")
- ("m v" TeX-view :name "view")
- ("m f" TeX-master-file-ask :name "master file")
- ("m h" TeX-home-buffer :name "home buffer")
- ("m m" TeX-command-run-all :name "compile")
- ("m o" TeX-recenter-output-buffer :name "compile output")
- ("m k" TeX-kill-job :name "kill compilation")
- ("m d" preview-clearout-buffer :name "clear preview"))
-;; hints
-(which-key-add-major-mode-key-based-replacements 'TeX-mode
-  "m q" "fill")
+(which-key-add-key-based-replacements
+  "\\" "auctex"
+  "\\ q" "fill")
 
 ;; provide latex configuration
 (provide 'sk-latex)

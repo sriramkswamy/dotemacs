7bffeb318ac16dba12dfb488615ce1dc348feb76
Sriram Krishnaswamy <sriram.krish.92@gmail.com>
2017-12-29 16:34:09 -0800

    LSP improvements

--- lang/sk-lsp.el
+++ lang/sk-lsp.el
@@ -1,6 +1,9 @@
 ;; Microsoft Language Server Protocol (LSP)
 (use-package lsp-mode
   :ensure t
+  :hook ((java-mode . lsp-mode)
+		 (python-mode . lsp-mode)
+		 (c++-mode . lsp-mode))
   :config
   ;; bindings
   (ryo-modal-key "m ?" 'lsp-capabilities :mode 'lsp-mode :name "lsp capability"))
